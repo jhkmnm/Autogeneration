48e5e06fa983b1168b5aaa520f65ee4d5fe7a0b5
jhkmnm <jhkmnm_123@163.com>
2016-11-23 08:47:59 +0800

    o

---
+++ 姓名自动生成/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace 姓名自动生成
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(44, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "生成姓名快捷键:Q";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(44, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "生成身份证快捷键:A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(44, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "生成手机号快捷键:Z";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(92, 33);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "注册热键";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 254);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ 姓名自动生成/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace 姓名自动生成
+{
+    public partial class Form1 : Form
+    {
+        HotKeys h = new HotKeys();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        public static extern int SendMessage(int hwnd, int wMsg, int wParam, StringBuilder lParam);
+
+        private List<string> 姓名s = new List<string>();
+        private List<string> 身份证号s = new List<string>();
+        private List<string> 手机号s = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            checkBox1.Checked = true;
+            
+        }
+
+        private void Regist()
+        {
+            var v = h.Regist(this.Handle, 0, Keys.Q, 姓名);
+            if (!v)
+            {
+                MessageBox.Show("注册失败！请重新启动");
+                return;
+            }
+            v = h.Regist(this.Handle, 0, Keys.A, 身份证号);
+            if (!v)
+            {
+                MessageBox.Show("注册失败！请重新启动");
+                return;
+            }
+            v = h.Regist(this.Handle, 0, Keys.Z, 手机号);
+            if (!v)
+            {
+                MessageBox.Show("注册失败！请重新启动");
+                return;
+            }
+        }
+
+        private void UnRegist()
+        {
+            h.UnRegist(this.Handle, 姓名);
+            h.UnRegist(this.Handle, 身份证号);
+            h.UnRegist(this.Handle, 手机号);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            //窗口消息处理函数
+            h.ProcessHotKey(m);
+            base.WndProc(ref m);
+        }
+
+        public void 姓名()
+        {
+            bool t = true;
+            string str = "";
+
+            while(t)
+            {
+                str = 自动生成.姓名();
+                if (!姓名s.Contains(str))
+                {
+                    t = false;
+                    姓名s.Add(str);
+                }
+            }
+
+            Copy(str);
+            Paste();
+        }
+
+        public void 身份证号()
+        {            
+            bool t = true;
+            string str = "";
+
+            while (t)
+            {
+                str = 自动生成.身份证号();
+                if (!身份证号s.Contains(str))
+                {
+                    t = false;
+                    身份证号s.Add(str);
+                }
+            }
+
+            Copy(str);
+            Paste();
+        }
+
+        public void 手机号()
+        {
+            bool t = true;
+            string str = "";
+
+            while (t)
+            {
+                str = 自动生成.手机号();
+                if (!手机号s.Contains(str))
+                {
+                    t = false;
+                    手机号s.Add(str);
+                }
+            }
+            
+            Copy(str);
+            Paste();
+        }
+
+        private void Paste()
+        {
+            IDataObject iData = Clipboard.GetDataObject();
+            string str = (String)iData.GetData(DataFormats.Text);
+            SendKeys.Send(str);//(String)iData.GetData(DataFormats.Text)
+        }
+
+        //复制
+        private void Copy(string str)
+        {
+            try
+            {
+                Clipboard.SetDataObject(str);
+            }
+            catch { }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                Regist();
+            else
+                UnRegist();
+        }
+    }
+}
